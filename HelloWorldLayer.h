14cf46668a250f2e93ce2dcf97d17bd954875869
iD Student Account <Student@idtechcamp-026.princeton.edu>
2011-07-06 11:45:39 -0400

    Updated Project

---
+++ HelloWorldLayer.h
@@ -0,0 +1,33 @@
+//
+//  HelloWorldLayer.h
+//  Traffic
+//  Car Project
+//
+//  Created by iD Student Account on 7/5/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+	CCSprite *trafficCar;
+	int trafficPositionX;
+	int trafficPositionY;
+	CGPoint pos;
+	int carA;
+	int carB;
+	int carC;
+	int carD;
+	CCSprite *myCar;
+	NSMutableArray *enemies;
+	CCSprite *roadWay;
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
